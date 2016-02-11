0d1c25346def9c33e2dd2ee85cd2c6526de6e49d
itang <live.tang@gmail.com>
2016-02-11 23:04:47 +0800

    cleanup

--- server/rust/deftype-server-rs/public/assets/js/index.js
+++ server/rust/deftype-server-rs/public/assets/js/index.js
@@ -1,60 +1,69 @@
-var api = (function(){
-    var ret = {
-        server: {
-            time: function(){
-                return $.getJSON("/api/server/time");
-            },
-            mode: function(){
-                return $.getJSON("/api/server/mode");
-            }
-        },
-        users: {
-            login: function(name, password){
-                var login_form ={login_name: name, password: password};
-                return $.ajax({
-                    url: "/api/users/login",
-                    data: JSON.stringify(login_form),
-                    contentType :   'application/json',
-                    method: "POST",
-                    dataType: "json"
-                });
-            }
+var api =
+    (function() {
+        function json_post(url, data) {
+            return $.ajax({
+                url: url,
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                method: "POST",
+                dataType: "json"
+            })
         }
-    };
 
-    return ret;
-})();
+        return {
+            server: {
+                time: function() {
+                    return $.getJSON("/api/server/time");
+                },
+                mode: function() {
+                    return $.getJSON("/api/server/mode");
+                }
+            },
+            users: {
+                login: function(name, password) {
+                    var login_form = {
+                        login_name: name,
+                        password: password
+                    };
+                    return json_post("/api/users/login", login_form);
+                }
+            }
+        };
+    })();
 
-$(function(){
+$(function() {
     var token = new Date();
-    $.ajaxSetup( {beforeSend: function(jqXHR) {
-        jqXHR.setRequestHeader("Authorization", "Bearer " + token);
-        console.log(token);
-    } } );
+    $.ajaxSetup({
+        beforeSend: function(jqXHR) {
+            jqXHR.setRequestHeader("Authorization", "Bearer " + token);
+            console.log(token);
+        }
+    });
 
-   function load_server_time(){
-       api.server.time().done(function(ret){
-          $("#time").html("server time:" + ret.data.now);
-       });
-   }
+    function load_server_time() {
+        api.server.time().done(function(ret) {
+            $("#time").html("server time:" + ret.data.now);
+        });
+    }
 
-   api.users.login("admin", "123456").done(function(ret){
-      console.log(JSON.stringify(ret));
-      if(ret.ok){
-          token = ret.data.token;
-          $("#msg").load("/api");
+    api.users.login("admin", "123456").done(function(ret) {
+        console.log(JSON.stringify(ret));
+        if (ret.ok) {
+            token = ret.data.token;
 
-           setInterval(load_server_time, 1000);
+            $("#msg").load("/api");
 
-           api.server.mode().done(function(ret){
-              if(ret.data == "development"){
-                $("#dev").load("/dev/1.html").show();
-              }
-           });
-      }else{
+            setInterval(load_server_time, 1000);
 
-      }
-  }).fail(function( jqXHR, textStatus ) {
-      alert( "Request failed: " + textStatus );
-  });
+            api.server.mode().done(function(ret) {
+                if (ret.data == "development") {
+                    $("#dev").load("/dev/1.html").show();
+                }
+            });
+        } else {
+            //
+        }
+    }).fail(function(jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
 });
