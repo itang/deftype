45c03bf310b468a35e74af62d363ed9503ce8ec3
itang <live.tang@gmail.com>
2016-07-03 11:58:22 +0800

    swift vapor version

--- server/swift/deftype-server-swift-vapor/App/Middleware/RuntimeMiddleware.swift
+++ server/swift/deftype-server-swift-vapor/App/Middleware/RuntimeMiddleware.swift
@@ -4,14 +4,13 @@ import Foundation
 class RuntimeMiddleware: Middleware {
 
     func respond(to request: Request, chainingTo chain: Responder) throws -> Response {
-        let date = NSDate()
-        let start = date.timeIntervalSince1970
+        let start = NSDate().timeIntervalSince1970
 
         let response = try chain.respond(to: request)
 
-        let end = date.timeIntervalSince1970
+        let end = NSDate().timeIntervalSince1970
 
-        response.headers["XRuntime"] = "\(end - start)"
+        response.headers["XRuntime"] = "\((end - start) * 1000) ms"
 
         return response
     }
